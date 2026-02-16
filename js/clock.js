aa906a3f3db87e921fc85e3c9dfb05f816bcb8b5
Reachsak Ly <53211721+reachsak@users.noreply.github.com>
2026-02-15 21:26:36 -0600

    add
    add

--- js/clock.js
+++ js/clock.js
@@ -1,17 +1,14 @@
 $(document).ready(function () {
   let clock;
 
-  // Grab the current date
   let currentDate = new Date();
 
-  // Target future date/24 hour time/Timezone
+  // Wedding date
   let targetDate = moment.tz("2026-06-19 00:00", "Asia/Phnom_Penh");
 
-  // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
 
   if (diff <= 0) {
-    // If remaining countdown is 0
     clock = $(".clock").FlipClock(0, {
       clockFace: "DailyCounter",
       countdown: true,
@@ -19,7 +16,6 @@ $(document).ready(function () {
     });
     console.log("Date has already passed!");
   } else {
-    // Run countdown timer
     clock = $(".clock").FlipClock(diff, {
       clockFace: "DailyCounter",
       countdown: true,
@@ -30,19 +26,12 @@ $(document).ready(function () {
       },
     });
 
-    // Check when timer reaches 0, then stop at 0
-    setTimeout(function () {
-      checktime();
-    }, 1000);
+    setTimeout(checktime, 1000);
 
     function checktime() {
-      t = clock.getTime();
-      if (t <= 0) {
-        clock.setTime(0);
-      }
-      setTimeout(function () {
-        checktime();
-      }, 1000);
+      let t = clock.getTime();
+      if (t <= 0) clock.setTime(0);
+      setTimeout(checktime, 1000);
     }
   }
 });
